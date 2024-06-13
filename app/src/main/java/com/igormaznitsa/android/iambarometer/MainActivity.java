d7cd82768227102035c24df45d6bc44a49445603
Igor Maznitsa <rrg4400@gmail.com>
2024-06-13 11:28:46 +0300

    added reset of settings and indicator of zero altitude

--- app/src/main/java/com/igormaznitsa/android/iambarometer/MainActivity.java
+++ app/src/main/java/com/igormaznitsa/android/iambarometer/MainActivity.java
@@ -12,6 +12,7 @@ import android.text.InputType;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -32,34 +33,45 @@ public class MainActivity extends AppCompatActivity {
   private static final double PRESSURE_SEA_LEVEL_HPA = 1013.25d;
   private static final String PROPERTY_CALIBRATED_ALTITUDE_OFFSET_METERS =
       "altitude.calibrated.offset.meters";
+  private static final String PROPERTY_CALIBRATED_SEA_LEVEL_HPA =
+      "altitude.calibrated.sea.level.hpa";
+  private static final double TEMPERATURE_AVERAGE = 15.0d;
   private SensorManager sensorManager = null;
   private List<Sensor> sensorList = Collections.emptyList();
   private ImageView imageArrowView;
+  private ImageView imageSeaLevelView;
   private TextView textViewHpa;
   private TextView textViewMmHg;
   private TextView textViewAlt;
   private volatile double calibratedDeltaMeters = 0.0d;
-  private volatile float lastPressureHpa = Float.NEGATIVE_INFINITY;
-
+  private volatile float calibratedSeaZeroHpa = Float.NaN;
+  private volatile float lastPressureHpa = Float.NaN;
   private final SensorEventListener sensorEventListener = new SensorEventListener() {
     @Override
     public void onSensorChanged(SensorEvent event) {
       final float valueHpa = event.values[0];
       lastPressureHpa = valueHpa;
-      final float indicatorPressureHpa = Math.min(MAX_MEASURABLE_PRESSURE_HPA, Math.max(
-          MIN_MEASURABLE_PRESSURE_HPA, valueHpa));
-      final float rotateAngle = ((indicatorPressureHpa - MIN_MEASURABLE_PRESSURE_HPA) / (
-          MAX_MEASURABLE_PRESSURE_HPA -
-              MIN_MEASURABLE_PRESSURE_HPA)) * SCALE_MAX_ANGLE;
+      final float rotateAngle = calcIndicatorRotateAngleForHpa(valueHpa);
 
       final ImageView locImageView = imageArrowView;
+      final ImageView locImageSeaLevelView = imageSeaLevelView;
       final TextView locTextViewHpa = textViewHpa;
       final TextView locTextViewMmHg = textViewMmHg;
       final TextView locTextViewAlt = textViewAlt;
+      final float locCalibratedSeaZeroHpa = calibratedSeaZeroHpa;
 
       if (locImageView != null) {
         runOnUiThread(() -> {
           imageArrowView.setRotation(rotateAngle);
+          if (locImageSeaLevelView != null) {
+            if (Float.isNaN(locCalibratedSeaZeroHpa)) {
+              locImageSeaLevelView.setVisibility(View.INVISIBLE);
+            } else {
+              locImageSeaLevelView.setVisibility(View.VISIBLE);
+              locImageSeaLevelView.setRotation(
+                  calcIndicatorRotateAngleForHpa(locCalibratedSeaZeroHpa));
+            }
+          }
           if (locTextViewHpa != null) {
             locTextViewHpa.setText(String.format(Locale.ENGLISH, "%.2f hPa", valueHpa));
           }
@@ -68,9 +80,11 @@ public class MainActivity extends AppCompatActivity {
                 String.format(Locale.ENGLISH, "%.2f mmHg", convertHpaTommHg(valueHpa)));
           }
           if (locTextViewAlt != null) {
-            Log.w("test", "test: delta " + calibratedDeltaMeters);
-            locTextViewAlt.setText(String.format(Locale.ENGLISH, "%.2f m",
-                calcRawAltitudeMeters(valueHpa) + calibratedDeltaMeters));
+            final double currentCalibration = calibratedDeltaMeters;
+            final double altitude = Double.isNaN(currentCalibration) ? calcRawAltitudeMeters(valueHpa) :
+                (calcRawAltitudeMeters(valueHpa) + currentCalibration);
+
+            locTextViewAlt.setText(String.format(Locale.ENGLISH, "%.2f m", altitude));
           }
         });
       }
@@ -82,12 +96,24 @@ public class MainActivity extends AppCompatActivity {
     }
   };
 
+  private static float calcIndicatorRotateAngleForHpa(final float hpa) {
+    final float indicatorPressureHpa = Math.min(MAX_MEASURABLE_PRESSURE_HPA, Math.max(
+        MIN_MEASURABLE_PRESSURE_HPA, hpa));
+    return ((indicatorPressureHpa - MIN_MEASURABLE_PRESSURE_HPA) / (
+        MAX_MEASURABLE_PRESSURE_HPA -
+            MIN_MEASURABLE_PRESSURE_HPA)) * SCALE_MAX_ANGLE;
+  }
+
   private static float convertHpaTommHg(final float hpa) {
     return hpa * 0.750063755419211f;
   }
 
+  private static double calcRawHpaForMeters(final double meters) {
+    return Math.pow(10.0d, Math.log10(PRESSURE_SEA_LEVEL_HPA) - (meters / 18400.0d / (1 + TEMPERATURE_COEFF * TEMPERATURE_AVERAGE)));
+  }
+
   private static double calcRawAltitudeMeters(final float hpa) {
-    return (18400.0d * (1 + TEMPERATURE_COEFF * 15.0d) * Math.log10(PRESSURE_SEA_LEVEL_HPA / hpa));
+    return (18400.0d * (1 + TEMPERATURE_COEFF * TEMPERATURE_AVERAGE) * Math.log10(PRESSURE_SEA_LEVEL_HPA / hpa));
   }
 
   @Override
@@ -103,6 +129,7 @@ public class MainActivity extends AppCompatActivity {
     try {
       super.onResume();
       this.imageArrowView = findViewById(R.id.imageArrow);
+      this.imageSeaLevelView = findViewById(R.id.imageSeaIndicator);
       this.textViewHpa = findViewById(R.id.textViewHpa);
       this.textViewMmHg = findViewById(R.id.textViewMmHg);
       this.textViewAlt = findViewById(R.id.textViewAlt);
@@ -131,9 +158,15 @@ public class MainActivity extends AppCompatActivity {
     alert.setPositiveButton(getText(R.string.ok), (dialog, whichButton) -> {
       try {
         final String text = input.getText().toString().trim();
-        if (!text.isEmpty()) {
+        if (text.isEmpty()) {
+          resetPreferences();
+        } else {
+          final float detectedPressureHpa = this.lastPressureHpa;
           final int value = Integer.parseInt(input.getText().toString());
-          savePreferences(value);
+          final double altitudeMeters = calcRawAltitudeMeters(detectedPressureHpa);
+          final double deltaMeters = value - altitudeMeters;
+          final float seaLevelPressure = (float) calcRawHpaForMeters(altitudeMeters - value);
+          savePreferences(deltaMeters, seaLevelPressure);
         }
       } catch (Exception ex) {
         Toast.makeText(this, "Error during number enter", Toast.LENGTH_SHORT).show();
@@ -153,6 +186,7 @@ public class MainActivity extends AppCompatActivity {
     } finally {
 
       this.imageArrowView = null;
+      this.imageSeaLevelView = null;
       this.textViewAlt = null;
       this.textViewMmHg = null;
       this.textViewHpa = null;
@@ -174,24 +208,41 @@ public class MainActivity extends AppCompatActivity {
 
   private void loadPreferences() {
     final SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+    this.calibratedSeaZeroHpa =
+        sharedPreferences.getFloat(PROPERTY_CALIBRATED_SEA_LEVEL_HPA, Float.NaN);
     this.calibratedDeltaMeters =
         sharedPreferences.contains(PROPERTY_CALIBRATED_ALTITUDE_OFFSET_METERS) ?
             Double.longBitsToDouble(
-                sharedPreferences.getLong(PROPERTY_CALIBRATED_ALTITUDE_OFFSET_METERS, 0L)) : 0.0d;
+                sharedPreferences.getLong(PROPERTY_CALIBRATED_ALTITUDE_OFFSET_METERS, Double.doubleToLongBits(Double.NaN))) : Double.NaN;
     Log.i(MainActivity.class.getSimpleName(),
         "Loaded preferences: calibratedDeltaMeters=" + this.calibratedDeltaMeters);
   }
 
-  private void savePreferences(final int altitudeMeters) {
-    final double deltaMeters = altitudeMeters - calcRawAltitudeMeters(this.lastPressureHpa);
-    Log.i(MainActivity.class.getSimpleName(),
-        "Save delta: " + deltaMeters + " m, for " + altitudeMeters + 'm');
-
+  private void resetPreferences() {
     try {
-      this.calibratedDeltaMeters = deltaMeters;
+      this.calibratedDeltaMeters = Double.NaN;
+      this.calibratedSeaZeroHpa = Float.NaN;
       final SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
       sharedPreferences.edit()
-          .putLong(PROPERTY_CALIBRATED_ALTITUDE_OFFSET_METERS, Double.doubleToLongBits(deltaMeters))
+          .clear()
+          .apply();
+    } catch (Exception ex) {
+      Toast.makeText(this, "Error during property reset", Toast.LENGTH_LONG).show();
+      Log.e(MainActivity.class.getSimpleName(), "Can't reset preferences", ex);
+    }
+  }
+
+  private void savePreferences(
+      final double deltaMeters, final float calibratedZeroSeaLevelHpa) {
+    try {
+      this.calibratedDeltaMeters = deltaMeters;
+      this.calibratedSeaZeroHpa = calibratedZeroSeaLevelHpa;
+      final SharedPreferences sharedPreferences = getPreferences(Context.MODE_PRIVATE);
+
+      sharedPreferences.edit()
+          .putLong(PROPERTY_CALIBRATED_ALTITUDE_OFFSET_METERS,
+              Double.doubleToLongBits(deltaMeters))
+          .putFloat(PROPERTY_CALIBRATED_SEA_LEVEL_HPA, calibratedZeroSeaLevelHpa)
           .apply();
     } catch (Exception ex) {
       Toast.makeText(this, "Error during property save", Toast.LENGTH_LONG).show();
